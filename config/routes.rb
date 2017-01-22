cc795f2ad09de66bec0fd022146dcde3b5725178
Alex Macpherson <alexjamesmacpherson@gmail.com>
2017-01-22 01:32:59 +0000

    Schools and Users modelled, one-to-many relation established.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'schools/index'
+
+  get 'schools/show'
+
+  get 'schools/new'
+
+  get 'schools/create'
+
+  get 'schools/edit'
+
+  get 'schools/update'
+
   root 'static#home'
 
   get '/about'    =>  'static#about'
