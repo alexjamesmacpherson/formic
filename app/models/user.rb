22ed6bfc26915bc17e4ea08a0e0293f7463b0516
Alex Macpherson <alexjamesmacpherson@gmail.com>
2017-02-16 21:00:20 +0000

    Password resets added to users, users and schools now complete bar styling.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :children, :class_name => 'Parent', :foreign_key => 'child_id', dependent: :destroy
 
   # Attribute accessors
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   # Remove whitespace
   auto_strip_attributes :email, :name, :address, :bio, :squish => true
@@ -82,6 +82,19 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
 private
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test 'account_activation' do
+  test 'account activation' do
     user = users(:test_user)
     school = School.find(user.school_id)
     user.activation_token = User.new_token
@@ -14,6 +14,20 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.name, mail.body.encoded
     assert_match school.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
+
+  test 'password reset' do
+    user = users(:test_user)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+
+    assert_equal 'Reset Your Password', mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ['noreply@formic.herokuapp.com'], mail.from
+
+    assert_match user.name, mail.body.encoded
+    assert_match user.reset_token, mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
   end
 end
