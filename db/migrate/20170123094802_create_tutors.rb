f5ad8a3cf7f836b8a88e4e5bd9f62e169af54a5f
Alex Macpherson <alexjamesmacpherson@gmail.com>
2017-02-16 22:18:17 +0000

    Heroku PostgreSQL referential relations hotfix.

--- db/migrate/20170123094802_create_tutors.rb
+++ db/migrate/20170123094802_create_tutors.rb
@@ -1,11 +1,13 @@
 class CreateTutors < ActiveRecord::Migration[5.0]
   def change
     create_table :tutors do |t|
-      t.references :tutor, foreign_key: true
-      t.references :pupil, foreign_key: true
+      t.references :tutor
+      t.references :pupil
 
       t.timestamps
     end
     add_index :tutors, [:tutor_id, :pupil_id], unique: true
+    add_foreign_key :tutors, :users, column: :tutor_id
+    add_foreign_key :tutors, :users, column: :pupil_id
   end
 end
--- db/migrate/20170123100225_create_parents.rb
+++ db/migrate/20170123100225_create_parents.rb
@@ -1,11 +1,13 @@
 class CreateParents < ActiveRecord::Migration[5.0]
   def change
     create_table :parents do |t|
-      t.references :parent, foreign_key: true
-      t.references :child, foreign_key: true
+      t.references :parent
+      t.references :child
 
       t.timestamps
     end
     add_index :parents, [:parent_id, :child_id], unique: true
+    add_foreign_key :parents, :users, column: :parent_id
+    add_foreign_key :parents, :users, column: :child_id
   end
 end
