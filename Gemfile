d5b4323850be074ac9abb04d3a248d785d26a93a
Alex Macpherson <alexjamesmacpherson@gmail.com>
2017-03-02 18:55:09 +0000

    Integrated CodeClimate with repo and Travis CI for test coverage and GPA. Other minor improvements. (#9)
    * Enabled CodeClimate test coverage and integrated with Travis CI.
    
    * Updated README
    
    * Updated .gitignore
    
    * Improved test coverage.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use Puma as the app server
@@ -53,6 +52,8 @@ end
 group :test do
   gem 'rake'
   gem 'rails-controller-testing'
+  gem 'simplecov'
+  gem 'codeclimate-test-reporter', '~> 1.0.0'
 end
 
 group :production do
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user)
+  end
+
   test 'should get new' do
     get login_path
     assert_response :success
+  end
+
+  test 'should redirect to user if logged in' do
+    log_in_as @user
+    assert is_logged_in?
+
+    get login_path
+    assert_response :redirect
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
